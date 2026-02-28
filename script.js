c721ee063e01cf0966825686f39fbb12f592842b
nhtseri52 <nhtseri52@gmail.com>
2026-02-28 20:21:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const firebaseConfig = { 
   apiKey: "AIzaSyCrgepkYAgTAniQBrDRRqis470Aea6Stk4", 
-  authDomain: "speedtype-pro-f0b75.firebaseapp.com", 
-  projectId: "speedtype-pro-f0b75", 
-  storageBucket: "speedtype-pro-f0b75.firebasestorage.app", 
-  messagingSenderId: "121414853341", 
-  appId: "1:121414853341:web:504c3f9f36b03329cfb134" 
+  authDomain: "flashtype.vn", 
+  projectId: "speedtype-pro-f0b75",
+  storageBucket: "speedtype-pro-f0b75.firebasestorage.app",
+  messagingSenderId: "121414853341",
+  appId: "1:121414853341:web:504c3f9f36b03329cfb134"
 };
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
